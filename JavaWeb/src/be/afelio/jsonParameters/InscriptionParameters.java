c29b37241749b9a349b352dfdf63d0b31f7fb441
Damien Bouffioux <damien.bouffioux@gmail.com>
2019-08-05 16:09:12 +0200

    Refactored names in api

--- JavaWeb/src/be/afelio/jsonParameters/InscriptionParameters.java
+++ JavaWeb/src/be/afelio/jsonParameters/InscriptionParameters.java
@@ -3,20 +3,20 @@ package be.afelio.jsonParameters;
 
 public class InscriptionParameters {
 
-	protected Integer activity_id;
-	protected Integer person_id;
+	protected Integer activityId;
+	protected Integer personId;
 	
-	public Integer getActivity_id() {
-		return activity_id;
+	public Integer getActivityId() {
+		return activityId;
 	}
-	public void setActivity_id(Integer activity_id) {
-		this.activity_id = activity_id;
+	public void setActivityId(Integer activityId) {
+		this.activityId = activityId;
 	}
-	public Integer getPerson_id() {
-		return person_id;
+	public Integer getPersonId() {
+		return personId;
 	}
-	public void setPerson_id(Integer person_id) {
-		this.person_id = person_id;
+	public void setPersonId(Integer personId) {
+		this.personId = personId;
 	}
 	
 	
