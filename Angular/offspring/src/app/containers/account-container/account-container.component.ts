3640482ae4109e6305a066c8712755dc6636d320
Jean Leclercq <jean_leclercq@outlook.com>
2019-08-05 15:22:24 +0200

    add containers in new project

---
+++ Angular/offspring/src/app/containers/account-container/account-container.component.ts
@@ -0,0 +1,194 @@
+import { Component, OnInit, Output, Input } from '@angular/core';
+import { ActivitiesService } from 'src/app/services/activities.service';
+import { Activity } from 'src/app/models/activity.model';
+import { Person } from 'src/app/models/person.models';
+import { EventService } from 'src/app/services/event.service';
+import { Event } from 'src/app/models/event.model';
+import { Inscription } from 'src/app/models/inscription.model';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { InscriptionService } from 'src/app/services/inscription.service';
+import { PersonService } from '../../services/person.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-account-container',
+  templateUrl: './account-container.component.html',
+  styleUrls: ['./account-container.component.scss']
+})
+
+export class AccountContainerComponent implements OnInit {
+
+  public activities: Activity[];
+  public activity: Activity;
+  public person: Person;
+  public persons: Person[];
+  public event: Event;
+  public events: Event[];
+  public inscriptions: Inscription[];
+  public isDeleted: boolean;
+  // tabs
+  public showInfoTab: boolean;
+  public showAccountUpdateForm: boolean;
+  public tabsStatuses: any;
+  public tabActive: any;
+  // event-form pop-up
+  public showCreateEventPopup: boolean;
+  public showActivityPopup: boolean;
+  public activityToShow: Activity;
+  public isManagement: boolean;
+  public showCreateActivityPopup: boolean;
+
+  constructor(
+    private activitiesService: ActivitiesService,
+    private eventService: EventService,
+    private authService: AuthenticationService,
+    private inscriptionService: InscriptionService,
+    private personService: PersonService,
+    private router: Router) {
+    this.activity = new Activity();
+    // set default tabs status
+    this.showInfoTab = true;
+    this.tabActive = 'info';
+    this.showAccountUpdateForm = false;
+    this.showCreateActivityPopup = false;
+  }
+
+  ngOnInit() {
+    this.person = this.authService.getPerson();
+    this.initActivities();
+    this.initEvents();
+    this.initInscriptions();
+  }
+
+  public initActivities() {
+    this.activitiesService.getAllActivitiesToManage(this.person.id)
+      .subscribe(activities => {
+        this.activities = activities;
+      });
+  }
+
+  public initEvents() {
+    this.eventService.getEventByPersonId(this.person.id)
+      .subscribe(events => {
+        this.events = events;
+      });
+  }
+
+  public initInscriptions() {
+    this.inscriptionService.getAllInscriptionsForOnePerson(this.person.id).subscribe(inscription => {
+      this.inscriptions = inscription;
+    });
+  }
+
+  public createActivity(activity: Activity) {
+    this.activitiesService.createActivity(activity).subscribe(() => {
+      console.log('dans le oncreateActivity');
+      this.initActivities();
+
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  public createEvent(event: Event) {
+    console.log('dans container');
+    console.log(event.name);
+    event.person_id = this.person.id;
+    this.eventService.createEvent(event).subscribe(() => {
+      this.initEvents();
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  public deleteEvent(eventId: number) {
+    this.eventService.deleteEvent(eventId).subscribe(() => {
+      this.isDeleted = true;
+      this.initEvents();
+      this.initActivities();
+    }, error => {
+      console.log(error);
+    });
+  }
+  public deleteActivity(activity: Activity) {
+    console.log('deleteActivity avant Service ' + activity);
+
+    this.activitiesService.deleteActivity(activity.id).subscribe(() => {
+      this.initActivities();
+      this.isDeleted = true;
+      console.log('dans le deleteActivity');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  public deleteInscription(inscription: Inscription) {
+    this.inscriptionService.deleteInscription(inscription.id).subscribe(() => {
+      console.log('OK');
+      this.isDeleted = true;
+      this.initInscriptions();
+    }, error => {
+      this.isDeleted = false;
+      console.log(error);
+    });
+  }
+  public updateProfile(personUpdated: Person) {
+    console.log('container  : ' + personUpdated.firstname);
+
+    this.personService.updatePerson(personUpdated).subscribe(() => {
+      console.log('OK');
+      localStorage.setItem('Person', JSON.stringify(personUpdated));
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  public deleteProfile() {
+    this.person = JSON.parse(localStorage.getItem('Person'));
+    this.personService.deleteProfile(this.person.id).subscribe(() => {
+      this.authService.logout();
+      this.router.navigate(['/home']);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  public toggleTab(tab: string) {
+    this.resetTab(this.tabActive);
+    // switch tab status
+    switch (tab) {
+      case 'info':
+        this.showInfoTab = true;
+        this.tabActive = tab;
+        break;
+      case 'account-update-form':
+        this.showAccountUpdateForm = true;
+        this.tabActive = tab;
+        break;
+    }
+  }
+
+  public resetTab(tabActive: string) {
+    switch (tabActive) {
+      case 'info':
+        this.showInfoTab = false;
+        break;
+      case 'account-update-form':
+        this.showAccountUpdateForm = false;
+        break;
+    }
+  }
+  public toggleActivityItem(activity: Activity) {
+    this.isManagement = true;
+    this.showActivityPopup = !this.showActivityPopup;
+    this.activityToShow = activity;
+    this.isManagement = true;
+  }
+  public toggleCreateEventPopup() {
+    this.showCreateEventPopup = !this.showCreateEventPopup;
+  }
+
+  public toggleCreateActivityPopup() {
+    this.showCreateActivityPopup = !this.showCreateActivityPopup;
+  }
+}
---
+++ Angular/offspring/src/app/containers/menu-container/menu-container.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Person } from 'src/app/models/person.models';
+import { PersonService } from '../../services/person.service';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+
+@Component({
+  selector: 'app-menu-container',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+
+export class MenuContainerComponent implements OnInit {
+
+  public showMenuProfile: boolean;
+  public showPopupProfile: boolean;
+  public person: Person;
+  public isCreate: boolean;
+
+
+  constructor(
+    private login: LoginService,
+    private authService: AuthenticationService,
+    private personService: PersonService,
+    private router: Router) {
+    this.showMenuProfile = false;
+    this.showPopupProfile = false;
+  }
+
+  ngOnInit() {
+  }
+
+  public isLogged(): boolean {
+    return this.authService.isLogged();
+  }
+
+  public getPerson(): Person {
+    return this.authService.getPerson();
+  }
+
+  public toggleMenuProfile() {
+    this.showMenuProfile = !this.showMenuProfile;
+  }
+
+  public togglePopupProfile() {
+    console.log('togglePopupProfile');
+
+    this.showPopupProfile = !this.showPopupProfile;
+  }
+
+  public logout() {
+    this.authService.logout();
+  }
+
+  private setLocalStorage() {
+    if (this.person !== null) {
+      localStorage.setItem('Authorization', 'true');
+      localStorage.setItem('Person', JSON.stringify(this.person));
+      console.log(localStorage.getItem('Authorization'));
+
+    } else {
+      localStorage.setItem('Authorization', 'false');
+      console.log(localStorage.getItem('Authorization'));
+    }
+  }
+
+  public onCreate() {
+    this.personService.createPerson(this.person).subscribe(() => {
+      console.log('OK');
+      this.isCreate = true;
+      this.setLocalStorage();
+      this.login.getConnection(this.person.login, this.person.password).subscribe();
+      this.router.navigate(['/home']);
+    }, error => {
+      console.log(error);
+    });
+  }
+}
