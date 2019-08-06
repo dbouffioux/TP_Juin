d9b7526a0367e4c775a66c301d8e9df7c88b0e85
Maxime Quoilin <max.quoilin@gmail.com>
2019-08-06 08:48:58 +0200

    bug css fixed

--- Angular/tp-juin/src/app/components/activity-form/activity-form.component.ts
+++ Angular/tp-juin/src/app/components/activity-form/activity-form.component.ts
@@ -9,7 +9,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-activity-form',
   templateUrl: './activity-form.component.html',
-  styleUrls: ['./activity-form.component.css']
+  styleUrls: ['./activity-form.component.scss']
 })
 
 export class ActivityFormComponent implements OnInit {
