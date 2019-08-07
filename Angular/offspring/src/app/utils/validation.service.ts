bfa9a08e8513302238f38c6aeca2a40714bd594b
Damien Bouffioux <damien.bouffioux@gmail.com>
2019-08-07 13:18:24 +0200

    changed form fioelds validation text to frenchh

--- Angular/offspring/src/app/utils/validation.service.ts
+++ Angular/offspring/src/app/utils/validation.service.ts
@@ -5,10 +5,10 @@ export class ValidationService {
   static getValidatorErrorMessage(validatorName: string, validatorValue?: any) {
 
     const config = {
-      required: 'Required',
-      invalidEmailAddress: 'Invalid email address',
-      invalidPassword: 'Invalid password. Password must be at least 6 characters long, and contain a number.',
-      minlength: `Minimum length ${validatorValue.requiredLength}`
+      required: 'Requis',
+      invalidEmailAddress: 'Adresse email invalide',
+      invalidPassword: 'Le mot de passe doit être de minimum 6 caractères de long avec un chiffre.',
+      minlength: `Longueur minimum: ${validatorValue.requiredLength}`
     };
     return config[validatorName];
   }
@@ -26,7 +26,6 @@ export class ValidationService {
   static passwordValidator(control) {
 
     // {6,100}           - Assert password is between 6 and 100 characters
-
     // (?=.*[0-9])       - Assert a string has at least one number
     if (control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
       return null;
